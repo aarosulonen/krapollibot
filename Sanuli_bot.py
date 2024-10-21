1897df925636836d14e9e78d5fe77003d31b4ecc
Aaro Sulonen <aaro.sulonen@aalto.fi>
2024-10-21 19:14:40 +0300

    goofy

--- Sanuli_bot.py
+++ Sanuli_bot.py
@@ -100,6 +100,9 @@ async def start_krapollis(bot):
         current_time = datetime.datetime.now().strftime("%H:%M")
         aiocron.crontab('00 09 * * *', func=create_polls, args=[bot], start=True)
         await bot.send_message(5160204048, f"Bot started at {current_time}")
+        
+async def goofy_ahh(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('goofy-ahh-sounds.mp3', 'rb'))
 
 def main() -> None:
     
@@ -116,6 +119,7 @@ def main() -> None:
     application.add_handler(CommandHandler("force_poll", force_polls))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("current_time", current_time_tell))
+    application.add_handler(CommandHandler("moro", goofy_ahh))
 
     
     application.run_polling(allowed_updates=Update.ALL_TYPES)
